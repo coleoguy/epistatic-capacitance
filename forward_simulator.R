6584911fd48637e64597fb62b75b4832943662bd
Andresdbp <71858994+Andresdbp@users.noreply.github.com>
2024-11-07 10:32:59 -0600

    Update forward_simulator.R

--- forward_simulator.R
+++ forward_simulator.R
@@ -3,31 +3,21 @@
 ## April 16, 2024
 
 
-
-
 ###### Starting Conditions #########
-N <- 10 #population
+N <- 100 #population
 loci <- 10 #positions on the genome 
 mu <- 10^-5 #human mutation rate 10^-9 for an individual nucleotide
 baseval <- 10 # this is a base minimum value for our phenotype
 loci.imp <- sort(sample(2:loci, loci/2))
-opt <- 15
-iter <- 20
+opt <- 20
+sigma <- 3
+iter <- 1000
 ###### End Starting Conditions #########
 
 
 ###### FUNCTIONS #########
 
 
-Life_cycle <- function() {
-  GetPopulation()
-  MutatePop()
-  GetPheno()
-  GetFit()
-  PickParents()
-  GetGametes()
-  MakeFertilization()
-}
 
 GetPopulation <- function(N,loci){
   
@@ -94,16 +84,16 @@ GetFit <- function(obs, opt, sigma){
   denom <- (2 * sigma)^2
   w <- exp(-(numer / denom))
   return(w)
-  }
+}
 
 PickParents <- function(pop, w){
-    
-    mothers <- sample(1:(N/2), size = N, replace = TRUE, prob = w[1:(N/2)])
-    fathers <- sample((N/2 + 1):N, size = N, replace = TRUE, prob = w[(N/2 + 1):N])
-    
-    return(list(mothers = mothers, fathers = fathers))
-  }
   
+  mothers <- sample(1:(N/2), size = N, replace = TRUE, prob = w[1:(N/2)])
+  fathers <- sample((N/2 + 1):N, size = N, replace = TRUE, prob = w[(N/2 + 1):N])
+  
+  return(list(mothers = mothers, fathers = fathers))
+}
+
 GetGametes <- function(mothers, fathers, pop) {
   genotype_lookup <- data.frame(
     genotype = c(1, 2, 3, 4),
@@ -112,10 +102,10 @@ GetGametes <- function(mothers, fathers, pop) {
   )
   
   get_maternal_alleles <- function(pop, mothers) {
-    # Cqll the mothers and their genotypes
+    # Call the mothers and their genotypes
     genotype <- pop[mothers, ]
     
-  
+    
     matched_rows <- match(genotype, genotype_lookup$genotype)
     # Match rows of the genotype to the genotypes in the table 
     
@@ -131,7 +121,7 @@ GetGametes <- function(mothers, fathers, pop) {
     # Get the genotypes of the fathers
     genotype <- pop[fathers, ]
     
-   
+    
     matched_rows <- match(genotype, genotype_lookup$genotype)
     
     # Extract alleles using the matched rows
@@ -147,138 +137,48 @@ GetGametes <- function(mothers, fathers, pop) {
   # Get paternal alleles
   paternal_alleles <- get_paternal_alleles(pop, fathers)
   
-  
   return(list(
     maternal_alleles = maternal_alleles,
     paternal_alleles = paternal_alleles
   ))
 }
 
-GetGametes <- function(mothers, fathers, pop, mu1, beta1) {
-  genotype_lookup <- data.frame(
-    genotype = c(1, 2, 3, 4),
-    allele_1 = c(0, 0, 1, 1),
-    allele_2 = c(0, 1, 0, 1)
-  )
-  
-  get_maternal_alleles <- function(pop, mothers) {
-    genotype <- pop[mothers, ]
-    matched_rows <- match(genotype, genotype_lookup$genotype)
-    allele_1 <- genotype_lookup$allele_1[matched_rows]
-    allele_2 <- genotype_lookup$allele_2[matched_rows]
-    return(list(allele_1 = allele_1, allele_2 = allele_2))
-  }
-  
-  get_paternal_alleles <- function(pop, fathers) {
-    genotype <- pop[fathers, ]
-    matched_rows <- match(genotype, genotype_lookup$genotype)
-    allele_1 <- genotype_lookup$allele_1[matched_rows]
-    allele_2 <- genotype_lookup$allele_2[matched_rows]
-    return(list(allele_1 = allele_1, allele_2 = allele_2))
-  }
-  
-  # Get maternal alleles
-  maternal_alleles <- get_maternal_alleles(pop, mothers)
-  
-  # Get paternal alleles
-  paternal_alleles <- get_paternal_alleles(pop, fathers)
-  
-  # Add x Dom effect calculations
-  if (single.arch == "add.x.dom") {
-    # Compress the genome into vector of values 0-2
-    genvec <- colSums(pop)
-    genvec <- ifelse(genvec == 2, 2, ifelse(genvec == 1, 1, 0))  # Vector of 0, 1, 2
-    
-    # Compare the ith and i+10th values in the vector
-    mlgen <- paste(genvec[1:10], genvec[11:20], sep = ",")
-    lookup <- c("0,0" = 0.125, "0,1" = 0, "1,0" = -0.25, "1,1" = 0, 
-                "2,2" = -0.125, "2,1" = 0, "1,2" = 0.25, "2,0" = 0.125, "0,2" = -0.125)
-    
-    # Calculate opp as the mean of mapped lookup values
-    opp <- mean(lookup[mlgen])
-    
-    # Calculate single.trait
-    single.trait <- mu1 + opp * beta1
-  }
-  
-  return(list(
-    maternal_alleles = maternal_alleles,
-    paternal_alleles = paternal_alleles,
-    single.trait = single.trait  # Add the computed trait to the output
-  ))
-}
-### Error in GetGametes(mothers, fathers, pop) : 
-###object 'single.arch' not found
-
 MakeFertilization <- function(gametes){
   
-  zygotes <- paste0(gametes$maternal, "-", gametes$paternal)
+  zygotes <- paste0(gametes$maternal_alleles$allele_1, "-", gametes$paternal_alleles$allele_1)
   
   lookup <- data.frame(
     zygote = c("0-0", "0-1", "1-0", "1-1"),
     genotype = c(1, 2, 3, 4)
   )
   
-  genotype <- lookup[zygotes]
+  genotype <- lookup$genotype[match(zygotes, lookup$zygote)]
+  
+  # Create new population matrix with updated genotypes
+  new_pop <- matrix(genotype, nrow = length(zygotes), ncol = loci, byrow = TRUE)
+  return(new_pop)
 }
 
+SimulateGenerations <- function(N, loci, mu, baseval, loci.imp, opt, iter, sigma) {
+  pop <- GetPopulation(N, loci)
+  avg_phenos <- numeric(iter)
+  for (generation in 1:iter) {
+    pop <- MutatePop(pop, mu)
+    phenos <- GetPheno(pop, loci.imp, baseval)
+    avg_phenos[generation] <- mean(phenos)
+    w <- GetFit(phenos, opt, sigma)
+    parents <- PickParents(pop, w)
+    gametes <- GetGametes(parents$mothers, parents$fathers, pop)
+    pop <- MakeFertilization(gametes)
+    print(generation)
+  }
+  return(list(final_population = pop, avg_phenos = avg_phenos))
+}
 
-###### END FUNCTIONS ########
+# Run the simulation
+simulation_result <- SimulateGenerations(N, loci, mu, baseval, loci.imp, opt, iter, sigma)
+final_population <- simulation_result$final_population
+average_phenotypes <- simulation_result$avg_phenos
 
-
-
-
-###### Running Sims ##########
-pop <- GetPopulation(N = N, loci = loci)
-pop <- MutatePop(pop = pop, mu = mu)
-phenos <- GetPheno(pop = pop, loci.imp = loci.imp, baseval = baseval)
-w <- GetFit(obs=phenos, opt=opt, sigma=2)
-parents <- PickParents(pop,w)
-mothers <- parents$mothers
-fathers <- parents$fathers
-gametes <- GetGametes(mothers, fathers, pop)
-###### Running Sims ##########
-
-
-
-
-
-
-# TODO We need to determine if GeetPopulation is giving us what we want with regard to the 
-# number of mutations being entered into the population.
-  
-## Start GetGametes
-# This steps comes after we have picked parents to mate, since we only make
-# one "successfull" gamete for each time a parent mates.
-# There's a some moving pieces that need to work in this step:
-# 1) We need to select at which point recombination will happen, preferably
-#    having a different random recombination spot for each parent
-# 2) We need to select one strand to start making a gamete and a method to 
-#    switch strand after the recombination spot
-# 3) We need to actually create the gametes, which should contain 0 and 1 as
-#    as their genotype, with values taken from the parent genotype dependent
-#    on the strand and recombination spot
-#
-## Fertilization
-# Since we should have twice as many gametes, we need to pick two random gametes
-# and have them go trough a compressing step that will take the gametes from 0,1
-# genotypes with two row to a 1-4 genotype on a single row. This step results
-# in the next generation's initial population
-#
-#
-#### This are the steps required to get a population that can complete
-#### it's life cycle and change will happen due to drift and mutations.
-#### The steps bellow will allow us to apply selection to our population
-#### But if you finish all the steps above we should meet before we start
-#### working on them.
-#
-#
-## GetPhenotype Function
-# This requires mapping the genotype to some phenotype (let's not worry about this one yet)
-#
-## GetFitness Function
-# This requires a phenotype that will be compared to an optimum. Should also record population's mean fitness
-#
-# After this we will add the steps specific to our study (measuring epi and add
-# variance, making an optimum shift, creating a bottleneck, etc.)
+plot(average_phenotypes)
 
